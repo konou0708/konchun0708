2f34674619be221d49a940013415899c326e411d
konou0708 <103973949+konou0708@users.noreply.github.com>
2023-02-07 20:43:03 +0900

    Update ex06-10.js

--- ex06-10.js
+++ ex06-10.js
@@ -1,6 +1,6 @@
 function imgchange1() {
     document.getElementById('logo').src= 
-    './img/OIP (1).jpeg';
+    '202301050000208-w500_0.jpg';
     }
     function imgchange2() {
         document.getElementById('logo').src= 
