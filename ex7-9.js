8aec28d6ba8b69563428c284e8e4a34638482ba6
konou0708 <103973949+konou0708@users.noreply.github.com>
2023-02-07 20:40:50 +0900

    Update ex7-9.js

--- ex7-9.js
+++ ex7-9.js
@@ -1,8 +1,8 @@
 function nazo5(){
-if(document.getElementById('answer5').value=='村上宗隆'){
+if(document.getElementById('answer5').value=='アーロンジャッジ'){
  alert('あたり！');
 }
-else if(document.getElementById('answer5').value=='むらかみむねたか'){
+else if(document.getElementById('answer5').value=='アーロンジャッジ'){
  alert('あたり！');
 }
 else{
