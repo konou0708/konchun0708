493b048201f1594f390b235df11e198d74455063
konou0708 <103973949+konou0708@users.noreply.github.com>
2023-02-07 19:50:28 +0900

    Create ex06-10.js

---
+++ ex06-10.js
@@ -0,0 +1,8 @@
+function imgchange1() {
+    document.getElementById('logo').src= 
+    './img/OIP (1).jpeg';
+    }
+    function imgchange2() {
+        document.getElementById('logo').src= 
+        './img/OIP.jpeg';
+        }
